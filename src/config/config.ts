bdf9e73e5c6d0e4759410da4d501935752d8caa7
yaeoni <tjghwnd103@gmail.com>
2021-06-29 19:27:01 +0900

    [init] Initialize foldering

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+export default {
+  port: parseInt(process.env.PORT, 10),
+  mongoURI: process.env.MONGODB_URI,
+};
