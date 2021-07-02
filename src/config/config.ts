ad7aa1ea0947414c571eefa350d9a2e7d3bd1c9f
yaeoni <tjghwnd103@gmail.com>
2021-07-02 14:15:51 +0900

    [init] Set test database

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,7 +12,7 @@ if (envFound.error) {
 export default {
   username : process.env.DB_USERNAME || 'root',
   password : process.env.DB_PASSWORD,
-  database : process.env.DB_DBNAME || 'charo',
+  database : process.env.DB_DBNAME || 'charo_test',
   host : process.env.DB_HOST,
   port : process.env.DB_PORT,
   socketPath: '/var/run/mysqld/mysqld.sock',
