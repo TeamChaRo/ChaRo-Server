ea4ad5f7e51af8aca391db22bfc7bacfdd0cc592
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-04 15:16:29 +0900

    [feat] User,Theme,Warning 모델 구축

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import {
     DataTypes, 
     Model, 
 } from 'sequelize';
-import sequelize from './index';
+import {sequelize} from "../Loaders/db"
 
 // user 모델의 구성요소를 명시
 interface UserAttributes {
@@ -14,7 +14,7 @@ interface UserAttributes {
     pushAlarm: number;
 };
 
-export class User extends Model<UserAttributes> {
+export default class User extends Model<UserAttributes> {
     //조회 후 사용 되어질 요소들의 타입명시 설정이 되어 있지 않으면 조회시 또는 조회 후 데이터 타입체크에서 오류
     public userId!: string;
     public password!: string;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
 import { sequelize } from "../Loaders/db"
-import Theme from "./theme";
-import Warning from "./warning";
+import Theme from "./Theme";
+import Warning from "./Warning";
+import User from "./User";
 
 export const db = { 
   Sequelize,
@@ -10,4 +11,5 @@ export const db = {
   // Tables
   Theme, 
   Warning,
+  User,
 };
