c00db7fb50210d485e7aee1b1839a93b80cd460a
yaeoni <tjghwnd103@gmail.com>
2021-07-06 01:41:30 +0900

    [chore] #3 개수 제한 에러 핸들링 & 비즈니스 로직 분리 필요

--- src/api/image.ts
+++ src/api/image.ts
@@ -11,49 +11,44 @@ import upload from "../middleware/upload";
 
 router.post(
     "/",
-    upload.single('image'), // 'image' key 값으로 맞춰야함
-    async(req: Request, res: Response)=>{
+    async(req: Request, res: Response) => {
         try{
-            // image path
-            let image = (req.file as Express.MulterS3.File).location;
-            console.log("successfully uploaded", image);
-
-            res.json({ "msg" : "success!"})
+            await upload.uploadSingle(req, res, function(err){
+                if(err){
+                    console.log("err!", err);
+                    return res.status(400).send("file upload error");
+                }
+                let image = (req.file as Express.MulterS3.File).location;
+                console.log("successfully uploaded", image);
+                return res.json({ "msg" : "success!"})
+            })
         }catch(err){
-            console.error(err.message);
+            console.log("err!!!", err);
             return res.status(500).send("Server error");
         }
     }
 )
 
+
+// Middleware X -> for error handling
 router.post(
     "/ary",
-    upload.array('image', 2), // 'image' key 값으로 맞춰야함
-    async(req: Request, res: Response)=>{
+    async(req: Request, res: Response) => {
         try{
-            for(let file of req.files){
-                // image path
-                let image = (file as Express.MulterS3.File).location;
-                console.log(image);
-            }
-            res.json({ "msg" : "success!"})
+            await upload.uploadAry(req, res, function(err){
+                if(err){
+                    console.log("err!", err);
+                    return res.status(400).send("file upload error");
+                }
+                for(let file of req.files){
+                    // image path
+                    let image = (file as Express.MulterS3.File).location;
+                    console.log(image);
+                }
+                return res.json({ "msg" : "success!"})
+            })
         }catch(err){
-            console.error(err.message);
-            return res.status(500).send("Server error");
-        }
-    }
-)
-
-router.post(
-    "/fields",
-    upload.fields([ {name:'image1'}, {name:'image2'}]), // 'image' key 값으로 맞춰야함
-    async(req: Request, res: Response)=>{
-        try{
-            let obj = JSON.parse(JSON.stringify(req.file))
-            console.log(obj) 
-            res.json({ "msg" : "success!"})
-        }catch(err){
-            console.error(err.message);
+            console.log("err!!!", err);
             return res.status(500).send("Server error");
         }
     }
--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -11,16 +11,33 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-// 이미지 저장경로, 파일명 세팅
-export default multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: "test-yaewon",
-        contentType: multerS3.AUTO_CONTENT_TYPE, 
-        acl: 'public-read',
-        key: (req, file, cb) => {
-            cb(null, "test/"+file.originalname) 
-        },
-    }),
-    limits: { fileSize: 5 * 1024 * 1024 },
-});
+let upload = {
+    uploadSingle: multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: "test-yaewon",
+            contentType: multerS3.AUTO_CONTENT_TYPE, 
+            acl: 'public-read',
+            key: (req, file, cb) => {
+                cb(null, "test/"+file.originalname) 
+            },
+        }),
+        limits: { fileSize: 5 * 1024 * 1024 },
+    }).single('image'),
+
+    uploadAry: multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: "test-yaewon",
+            contentType: multerS3.AUTO_CONTENT_TYPE, 
+            acl: 'public-read',
+            key: (req, file, cb) => {
+                cb(null, "test/"+file.originalname) 
+            },
+        }),
+        limits: { fileSize: 5 * 1024 * 1024 },
+    }).array('image', 2),
+}
+
+export= upload;
+
