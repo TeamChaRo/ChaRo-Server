2b4800cc662c59562fda7d2458cebdacdaa126b9
yaeoni <tjghwnd103@gmail.com>
2021-07-06 14:13:50 +0900

    [feat] 메인, 더보기, 검색기록, 상세 조회 뷰 DTO 초안

---
+++ src/interface/res/detailDTO.ts
@@ -0,0 +1,22 @@
+export interface detailInformationDTO{
+    author: string,
+    profileImage: string,
+    date: string,
+    themes: [string],
+    
+    province: string,
+    city: string,
+
+    isParking: boolean,
+    parkingDesc: string,
+
+    courseDesc: string,
+
+    source: string,
+    wayPoint: [string],
+    longtitude: [string],
+    latitude: [string],
+    destination: string,
+
+    image: [string]
+}
