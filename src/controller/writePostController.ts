6a842c06b241af02c9f114351be7bd2cfb30159e
yaeoni <tjghwnd103@gmail.com>
2021-07-09 03:17:17 +0900

    [feat] 작성하기 api 완료(클라 파일 업로드 기능 이용 테스트 필요)

--- src/controller/writePostController.ts
+++ src/controller/writePostController.ts
@@ -1,30 +1,39 @@
 import { Request, Response } from "express";
-import imageUpload  from "./image";
+import writePostDTO from "../interface/req/writePostDTO";
+import writePostService from "../service/writePostService";
 
 export default async(req: Request, res: Response) => {
     try{
+        // images path
+        let imagesPath: string[] = [];
+ 
+        if(req.files){
+            for(let file of (req.files as Express.MulterS3.File[] ) ) 
+                imagesPath.push((file as Express.MulterS3.File).location);
+        }
 
-        let upload = imageUpload.postImages("1"); // userId
-        upload(req, res, function(err){
-            if(err){
-                console.log("err!", err);
-                return res.status(400).json({
-                    "success" : false,
-                    "msg" : "File Upload Error, 이미지 6개 제한"
-                });
-            }
-            for(let file of req.files){
-                // image path
-                let image = (file as Express.MulterS3.File).location;
-                console.log(image);
-            }
+        let postEntity: writePostDTO = {
+            title: req.body.title,
+            userId: req.body.userId,
+            courseImage: imagesPath,
             
-            // 완성
-            return res.status(200).json({ 
-                "success" : true,
-                "msg" : "success!"
-            })
-        })
+            province: req.body.province,
+            region: req.body.region,
+
+            theme: req.body.theme,
+            warning: req.body.warning,
+
+            isParking: req.body.isParking,
+            parkingDesc: req.body.parkingDesc,
+
+            courseDesc: req.body.courseDesc,
+
+            course: req.body.course
+        }
+        
+        const ret = await writePostService(postEntity);
+        return res.status(ret.status).json(ret.data);
+
     }catch(err){
         console.log("err!!!", err);
         return res.status(500).send("Server error");
