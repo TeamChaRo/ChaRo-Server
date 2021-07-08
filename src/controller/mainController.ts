699bed6266debeea9b0bd06e13a6a9a36b822e6b
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-08 20:14:26 +0900

    [feat] 메인뷰 로컬데이터 tags append 과정

--- src/controller/mainController.ts
+++ src/controller/mainController.ts
@@ -1,37 +1,10 @@
 import express, { Request, Response } from "express";
 import { validationResult } from "express-validator";
-import { localMain, themeMain, localStandard, themeStandard } from "../service/mainService";
-import { mainDTO } from '../interface/res/mainDTO'
-
-var localInfo: object 
-var localTitle: string 
-var mainData: mainDTO = {
-    banner: {
-        bannerImage: "",
-        bannerTags: "",
-        bannerTitle: ""
-    },
-    seasonTitle: '',
-    season: [{
-        title: '',
-        tags: [''],
-        image: '',
-        isFavorite: true
-    }],
-    localTitle: '',
-    local: [{
-        title: '',
-        tags: [''],
-        image: '',
-        isFavorite: false
-    }],
-}
+import { themeMain, themeStandard } from "../service/mainService";
+import getMain from "../service/mainService"
 
 export default async(req: Request, res: Response) => {
     // check error 핸들링(필요한가?)
-
-    // local, theme 테이블에서 기준되는 값 불러오기(안만듬) -> in service 에 정의하시게~
-    // ex)
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -42,44 +15,21 @@ export default async(req: Request, res: Response) => {
     }
     const { id } = req.body;
 
-    localStandard().then ( local => {
-        localTitle = local.localTitle
-        mainData.localTitle = localTitle
-        localMain(id, local.localCity).then(function(res) {
-            localInfo = res
-            console.log("로그로그",localInfo)
-            
-            //상위 네개만! -> 좋아요순으로 노출..!?
-            for (let key in localInfo) {
-                let titleValue:string = localInfo[key]['title'];
-                let favoriteValue = localInfo[key]['isFavorite']
-                if (favoriteValue == null) {
-                    favoriteValue = false
-                } 
-                else {
-                    favoriteValue = true
-                }
-                mainData.local[key] = {
-                    "title": titleValue,
-                    "tags": [""],
-                    "image": "",
-                    "isFavorite": favoriteValue
-                }
-            }
-        });    
-    });
+    // localStandard().then ( local => {
+    //     localMain(id, local.localCity)
+    // });
 
     themeStandard().then ( theme => {
-        themeMain(id, theme.standardTheme, theme.standardTitle).then( res => {
-            responseFunc()
+        themeMain(id, theme.standardTheme).then( res => {
+           // responseFunc()
         });
     })
-    
-    async function responseFunc() {
-        await res.status(200).json({
-        status: true,
-        message: "메인뷰 조회 성공",
-        data: mainData
+
+    const getMainService = await getMain(id);
+
+    res.status(200).json({
+            status: true,
+            message: "메인뷰 조회 성공",
+            data: getMainService.data
     });
-    }
 }
