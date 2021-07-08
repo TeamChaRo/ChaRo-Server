ded5512997e3684175166edf64466b6f83f4c5f0
yaeoni <tjghwnd103@gmail.com>
2021-07-08 13:10:19 +0900

    [Feat] 클라 논의 후, 이미지 업로드 middleware로 빼기(에러 핸들링 제외)

--- src/api/writePost.ts
+++ src/api/writePost.ts
@@ -1,16 +1,18 @@
 import express from "express";
 //import { check, validationResult } from "express-validator";
 import { writePostController } from "../controller";
+import upload from "../middleware/upload";
 const router = express.Router();
 
 
 /**
- *  @route Post /writeCourse
+ *  @route Post /writePost
  *  @desc write a post with images
  *  @access Public
  */
 router.post(
     "/",
+    upload.postImages,
     writePostController
 )
 
