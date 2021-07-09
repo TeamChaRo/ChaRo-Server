701c179009b818e068b5afdcb6eaf83b7f590ce1
yaeoni <tjghwnd103@gmail.com>
2021-07-09 17:20:34 +0900

    [feat] 더보기 controller-service 연결

--- src/controller/previewController.ts
+++ src/controller/previewController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-
+import previewService from "../service/previewService";
 export default async(req: Request, res: Response) => {
     try{
         console.log("preview Ctrl", req.params);
-
+        await previewService();
         return res.status(200).json({
             success: true,
             msg: "성공~"
