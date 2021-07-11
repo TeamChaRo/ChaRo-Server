4131cfc0c6f47d4ec2a0c32cdc61d81c750576ec
yaeoni <tjghwnd103@gmail.com>
2021-07-11 13:16:41 +0900

    [fix] index.ts 속 type 정의

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import logger from "morgan";
 const app = express();
 import connectDB from "./Loaders/connect";
@@ -16,7 +16,7 @@ app.use("/preview", require("./api/preview"));
 app.use("/postDetail", require("./api/postDetail"))
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "production" ? err : {};
