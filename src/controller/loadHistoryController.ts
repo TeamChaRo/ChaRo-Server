f8b90a0e161b3a7253ed9664c13e3e0c190fb5fe
yaeoni <tjghwnd103@gmail.com>
2021-07-12 05:16:00 +0900

    [feat] 지도 검색어 조회 api 기능 구현 / postman 테스트

--- src/controller/loadHistoryController.ts
+++ src/controller/loadHistoryController.ts
@@ -3,8 +3,9 @@ import express, { Request, Response } from "express";
 import { loadHistoryService } from "../service/searchHistoryService";
 
 export default async(req: Request, res: Response) => {
+    const userId = req.body.userId;
 
-    const result = await loadHistoryService();
+    const result = await loadHistoryService(userId);
     res.status(result.status).json(result.data);
 }
  
--- src/service/searchHistoryService.ts
+++ src/service/searchHistoryService.ts
@@ -2,6 +2,7 @@ import { db } from "../models";
 import { QueryTypes, Sequelize } from 'sequelize';
 
 import searchHistoryDTO from "../interface/req/searchHistoryDTO";
+import resHistoryDTO from "../interface/res/searchHistoryDTO";
 
 function insertFunction(entity: searchHistoryDTO){
     return new Promise((resolve) => {
@@ -54,13 +55,60 @@ export function saveHistoryService(history: searchHistoryDTO[]){
     
 }
 
-export async function loadHistoryService(){
+export async function loadHistoryService(userId: string){
 
-    return {
-        status: 200,
-        data:{
-            success : true,
-            msg : "아 성공~ 오예원한테 가서 먹을거 주고오삼",//"successfully load history",
+    try{
+
+        const result = await db.SearchHistory.findAll({ 
+            where:{userId:userId},
+            attributes: [
+                "title", "address", 
+                "latitude", "longitude", 
+                [Sequelize.fn('date_format', Sequelize.col('updatedAt'), '%Y-%m-%d'), 'date']
+            ], 
+            order: [['updatedAt', 'DESC']],
+            raw:true 
+        });
+
+        let history: resHistoryDTO[] = [];
+
+        result.forEach((value, index) => {
+            console.log(value['date']);
+
+            const date = (value['date'] as string).split("-");
+            
+            //console.log(value['updatedAt']);
+            const entity: resHistoryDTO = {
+                title: value.title,
+                address: value.address,
+                latitude: value.latitude,
+                longitude: value.longitude,
+                year: date[0],
+                month: date[1],
+                day: date[2]
+            }
+            history.push(entity);
+            //console.log("for Each!", entity);
+        })
+
+        return {
+            status: 200,
+            data:{
+                success : true,
+                msg : "아 성공~ 오예원한테 가서 먹을거 주고오삼",//"successfully load history",
+                data : history
+            }
+        }
+        
+    }catch(err){
+        console.log(err);
+        return {
+            status: 400,
+            data:{
+                success: false,
+                msg : "DB update fail"
+            }
         }
     }
+    
 }
