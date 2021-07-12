418630331294f036444d0ca241055cfa34860af5
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-13 04:03:55 +0900

    [feat] likeSearchService - 검색어가 3개일 때 구현완료 ✅
    검색어 3개 -> 지역,테마,주의사항 모두 포함일 때 ❗️

---
+++ src/service/likeSearchService.ts
@@ -0,0 +1,53 @@
+import { db } from "../models";
+import { QueryTypes } from 'sequelize';
+import briefInformationDTO from "../interface/res/briefInformationDTO";
+import searchInfoDTO from "../interface/res/searchInfoDTO";
+import searchDTO from "../interface/req/searchDTO";
+import { makeLocalBriefCollection } from "./briefCollectionService"
+
+//검색어가 3개모두일 때 서비스
+export async function rtwSearchService(searchDTO: searchDTO, userId: string){
+
+    //검색어가 3개일 때 쿼리 
+    const rtwSearchQuery = `SELECT count(liked_post.PostId) as favoriteCount, P.id as postId, P.title
+        FROM (SELECT id, title FROM post WHERE region = :region) AS P
+        LEFT OUTER JOIN liked_post ON(P.id = liked_post.PostId)
+        WHERE P.id IN (
+        SELECT P.id
+        FROM post_has_theme, post_has_warning
+        WHERE post_has_theme.postId = P.id AND post_has_theme.themeName = :theme AND post_has_warning.warningName = :warning and post_has_warning.postId = P.id
+        )
+        GROUP BY P.id ORDER BY favoriteCount DESC LIMIT 20`;
+    const rtwSearchRet = await db.sequelize.query(rtwSearchQuery,{ replacements:{ region: searchDTO.region[1], theme: searchDTO.theme, warning: searchDTO.warning },type: QueryTypes.SELECT });
+        
+        
+    let brief: briefInformationDTO[] = []
+
+    const searchInfo: searchInfoDTO = {
+        selectedFilter: [searchDTO.region[0], searchDTO.region[1], searchDTO.theme, searchDTO.warning], 
+        totalCourse: rtwSearchRet.length,
+        drive: brief
+    };
+
+    try{
+        await makeLocalBriefCollection(rtwSearchRet, brief, userId);
+        return {
+            status: 200,
+            data:{
+                success : true,
+                msg : "하이 ",
+                data : searchInfo
+            }
+        }
+
+    }catch(err){
+        console.log(err);
+        return {
+            status: 502,
+            data:{
+                success : false,
+                msg : "DB preview loading error"
+            }
+        }
+    }
+}
