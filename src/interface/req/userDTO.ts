10f1bd0f6b23dc8b64e77706378a7cf093fdd11f
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-12 20:35:13 +0900

    [feat] 회원가입 기능을 위한 userDTO 생성 (interface/req) ✅

---
+++ src/interface/req/userDTO.ts
@@ -0,0 +1,10 @@
+
+export default interface userDTO {
+    email: string;
+    id: string;
+    password: string;
+    profileImage: string;
+    nickname: string;
+    marketingPush: boolean;
+    marketingEmail:boolean;
+};
