5827d1842fc88474bb9839e76c0155f8643f49ac
yaeoni <tjghwnd103@gmail.com>
2021-07-13 01:58:40 +0900

    [feat] myPage DTO 작성

---
+++ src/interface/res/myPageDTO.ts
@@ -0,0 +1,29 @@
+export interface myPageUser{
+    nickname: string,
+    profileImage: string,
+    followging: string[],
+    follower: string[]
+}
+
+export interface myPagePreview{
+    title: string,
+    image: string,
+    tags: string[],
+    favoriteNum: number,
+    saveNum: number,
+    year: string,
+    month: string,
+    day: string
+}
+
+type myPagePreviewCollection = myPagePreview[];
+
+export interface myPageDTO{
+    userInformation: myPageUser,
+    
+    writtnTotal : number,
+    writtenPost: myPagePreviewCollection,
+    savedTotal : number,
+    savedPost: myPagePreviewCollection
+
+}
