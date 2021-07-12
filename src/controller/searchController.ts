f4b984baef224376ef41b1f844f8ec955a9bc79d
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-12 22:46:51 +0900

    [feat] searchByLikesController 생성 - req body로 받아올 좋아요순 search객체 만들고 try catch문 작성 ✏️

---
+++ src/controller/searchController.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import searchDTO from "../interface/req/searchDTO";
+
+/*
+searchByLikesController
+- 좋아요순 검색하기 controller
+*/
+export async function searchByLikesController(req: Request, res: Response) {
+    try {
+        let searchEntity: searchDTO = {
+            region: req.body.region,
+            theme: req.body.theme,
+            warning: req.body.warning
+        };
+
+    } catch {
+      res.status(500).json({
+        success: false,
+        msg: "서버 내부 오류",
+      });
+    }
+  }
