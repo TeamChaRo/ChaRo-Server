051d2083f11f2778176411ed7f0447f65d02caf7
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-13 04:00:08 +0900

    [feat] search/like 라우터 정의 - 좋아요순 👍🏻

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,3 +1,15 @@
 import express from "express";
-
+import { likeSearchController } from "../controller/searchController";
 const router = express.Router();
+
+/**
+ *  @route Post /search/like
+ *  @desc 좋아요순 검색하기
+ *  @access Public
+ */
+router.post(
+    "/like",
+    likeSearchController
+);
+
+module.exports = router;
