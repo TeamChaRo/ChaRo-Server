a98d5fbc29c34670783be2f23c7c881d31c978ca
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-12 20:38:12 +0900

    [feat] signController로 로그인, 회원가입, 이메일/닉네임 중복체크 컨트롤러 모두 관리하도록 로직 설계 및 구현 🛠
    - signController -> 유저 인증/회원가입 절차를 모아놓은 controller ✅

--- src/controller/signController.ts
+++ src/controller/signController.ts
@@ -1,35 +1,142 @@
 import express, { Request, Response } from "express";
-import { validationResult } from "express-validator";
 const router = express.Router();
-import signIn from "../service/signInService"
+import signIn from "../service/signInService";
+import signUp from "../service/signUpService";
+import checkId from "../service/idCheckService";
+import checkNickname from "../service/nicknameCheckService";
+import userDTO from "../interface/req/userDTO";
 
-
-export async function signInController (req: Request, res: Response) {
-  console.log("asfaf")
-  const errors = validationResult(req);
-    // check문 에러 핸들링 부분
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ 
-        status : 400,
-        //message : errors.array[], // 에러 핸들링 고민 필요!
-      });
-    }
+/*
+signInController
+- 로그인
+*/
+export async function signInController(req: Request, res: Response) {
+  try {
     const { id, password } = req.body;
-    console.log("id", id, "password", password)
-    const signInService = await signIn(id, password);
-    console.log("ddd",signInService);
-    return res.status(signInService.status).json(signInService.data);
+    const signInReturn = await signIn(id, password);
+
+    if (signInReturn.data == "noUserExist") {
+      return res.status(404).json({
+        success: false,
+        msg: "유저 없음",
+      })
+    }
+    if (signInReturn.data == "passwordError") {
+      return res.status(404).json({
+        success: false,
+        msg: "패스워드 오류",
+      })
+    }
+    return res.status(signInReturn.status).json(signInReturn.data);
+  } catch {
+    res.status(500).json({
+      success: false,
+      msg: "서버 내부 오류",
+    });
+  }
 }
 
-export async function signUpController (req: Request, res: Response) {
-  console.log("signUp")
-  
+/*
+signUpController
+- 회원가입
+*/
+export async function signUpController(req: Request, res: Response) {
+  try {
+
+    //let imagesPath: string = (req.file as Express.MulterS3.File).location;
+    let signUpEntity: userDTO = {
+      id: req.body.id,
+      password: req.body.password,
+      email: req.body.email,
+      nickname: req.body.nickname,
+      profileImage: req.body.profileImage,
+      marketingPush: req.body.pushAgree,
+      marketingEmail: req.body.emailAgree,
+    };
+
+    if (signUpEntity.profileImage == '') {
+      signUpEntity.profileImage = "https://charo-server.s3.ap-northeast-2.amazonaws.com/jieun.png"
+    }
+    if (signUpEntity.marketingPush ==  undefined) {
+      signUpEntity.marketingPush = false
+    }
+    if (signUpEntity.marketingEmail ==  undefined) {
+      signUpEntity.marketingEmail = false
+    }
+
+    const signUpReturn = await signUp(signUpEntity);
+    if (signUpReturn.data == "alreadyExistUser") {
+      return res.status(404).json({
+        success: false,
+        msg: "이미 존재하는 유저입니다.",
+      })
+    }
+    return res.status(signUpReturn.status).json(signUpReturn.data);
+
+  } catch {
+    res.status(500).json({
+      success: false,
+      msg: "서버 내부 오류",
+    });
+  }
 }
 
-export async function checkIdController (req: Request, res: Response) {
-  console.log("checkId")
+/*
+checkIdController
+- 아이디 중복체크
+*/
+export async function checkIdController(req: Request, res: Response) {
+  try {
+    const { id } = req.body;
+
+    var checkIdPw = /^[a-zA-Z0-9]{5,15}$/ 
+
+    if(!checkIdPw.test(id)) {
+      return res.status(404).json({
+        success: false,
+        msg: "아이디는 5~15자의 영문 대소문자와 숫자로만 입력해주세요.",
+      })
+    }
+
+    const checkIdReturn = await checkId(id);
+
+    if (checkIdReturn.data == "alreadyExistUser") {
+      return res.status(404).json({
+        success: false,
+        msg: "이미 존재하는 id입니다.",
+      })
+    }
+    return res.status(checkIdReturn.status).json(checkIdReturn.data);
+  } catch {
+    res.status(500).json({
+      success: false,
+      msg: "서버 내부 오류",
+    });
+  }
 }
 
-export async function checkNicknameController (req: Request, res: Response) {
-  console.log("checkNickname")
+/*
+checkNicknameController
+- 닉네임 중복체크
+*/
+export async function checkNicknameController(req: Request, res: Response) {
+  try {
+    const { nickname } = req.body;
+    const checkNicknameReturn = await checkNickname(nickname);
+
+    if (checkNicknameReturn.data == "alreadyExistNickname") {
+      return res.status(404).json({
+        success: false,
+        msg: "이미 존재하는 닉네임입니다.",
+      })
+    }
+    return res.status(checkNicknameReturn.status).json(checkNicknameReturn.data);
+  } catch {
+    res.status(500).json({
+      success: false,
+      msg: "서버 내부 오류",
+    });
+  }
 }
+
+module.exports
