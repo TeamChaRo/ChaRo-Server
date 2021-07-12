67048404295725cf8f17e7b2f4ba01ddbcb7561d
yaeoni <tjghwnd103@gmail.com>
2021-07-12 22:20:42 +0900

    [feat]
    좋아요 / 저장하기 API 기능 구현 및 postman 테스트 완료

--- src/controller/postController.ts
+++ src/controller/postController.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from "express";
 
-import { likePostService } from "../service/postService";
+import { likePostService, savePostService } from "../service/postService";
 
 export async function likePostController(req: Request, res: Response){
-    const userId = req.params.userId;
-    const postId = req.params.postId;
-
+    const userId = req.body.userId;
+    const postId = req.body.postId;
 
     const ret = await likePostService(userId, postId);
+    res.status(ret.status).json(ret.data);
+}
+
+export async function savePostController(req: Request, res: Response){
+    const userId = req.body.userId;
+    const postId = req.body.postId;
+
+    const ret = await savePostService(userId, postId);
     res.status(ret.status).json(ret.data);
 }
