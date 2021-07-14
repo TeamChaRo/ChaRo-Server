9037296555b79537dfbdb71b86fbcbe63a9d421e
yaeoni <tjghwnd103@gmail.com>
2021-07-15 06:02:20 +0900

    [feat] 실제 req.body 용 수정 / 테스트 필요

--- src/controller/writePostController.ts
+++ src/controller/writePostController.ts
@@ -4,39 +4,20 @@ import writePostService from "../service/writePostService";
 
 export default async(req: Request, res: Response) => {
     try{
-        const obj = {
-            title: '하이',
-            userId: 'injeong0418',
-            province: '특별시',
-            region: '서울',
-            theme: [ '여름', '산' ],
-            warning: [ 'true', 'true', 'false', 'false' ],
-            isParking: 'false',
-            parkingDesc: '예원아 새벽까지 고생이 많아',
-            courseDesc: '코스 드립크',
-            course:{
-              address: [ '123', '123' ],
-              latitude: [ '123', '123' ],
-              longtitude: [ '123', '123' ]
-            }
-        }
-        let warning: boolean[] = [];
-
-        //for(let idx in req.body.warning){
-        const rawWarning = obj.warning;
-        for(let idx in rawWarning){
-            if(rawWarning[idx] == "true") warning.push(true);
-            else warning.push(false);
-        }
-        
-
-        
         // images path
         let imagesPath: string[] = [];
  
         if(req.files){
             for(let file of (req.files as Express.MulterS3.File[] ) ) 
                 imagesPath.push((file as Express.MulterS3.File).location);
+        }
+
+        let warning: boolean[] = [];
+
+        const rawWarning = req.body.warning;
+        for(let idx in rawWarning){
+            if(rawWarning[idx] == "true") warning.push(true);
+            else warning.push(false);
         }
 
         let postEntity: writePostDTO = {
@@ -48,14 +29,14 @@ export default async(req: Request, res: Response) => {
             region: req.body.region,
 
             theme: req.body.theme,
-            warning: req.body.warning,
+            warning: warning,
 
             isParking: req.body.isParking,
             parkingDesc: req.body.parkingDesc,
 
             courseDesc: req.body.courseDesc,
 
-            course: obj.course//req.body.course
+            course: req.body.course
         }
         
         const ret = await writePostService(postEntity);
