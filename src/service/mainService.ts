b162468906cd191c29733a8b516df54d90ed9cfa
yaeoni <tjghwnd103@gmail.com>
2021-07-17 06:30:02 +0900

    [hotfix] today - local 동일하게 올려놓기

--- src/service/mainService.ts
+++ src/service/mainService.ts
@@ -92,67 +92,21 @@ export default async function mainService(userId: string, theme: string, region:
       nest: true,
     });
 
-    //today-> 추천알고리즘 들어가는데.
-    const todayPromise = new Promise(async (resolve, reject) => {
-      const query = `select P.id as postId, P.title as title, count(liked_post.PostId) as favoriteCount
-                    FROM (SELECT id, title FROM post) AS P
-                    LEFT OUTER JOIN liked_post ON(P.id = liked_post.PostId)
-                    GROUP BY P.id ORDER BY favoriteCount DESC LIMIT 4`;
+    const todayQuery = `SELECT P.id, P.title,  count(countLike.PostId) as favoriteCount, count(isLike.PostId) as isFavorite, 
+                            T.region, I.image1, T.region, T.theme, T.warning
+                            FROM (SELECT id, title FROM post WHERE region=:region) AS P
+                            LEFT OUTER JOIN liked_post as countLike ON(P.id = countLike.PostId)
+                            LEFT OUTER JOIN liked_post as isLike ON(isLike.PostId = P.id  and isLike.UserId =:userId)
+                            INNER JOIN post_has_image as I
+                            INNER JOIN post_has_tags as T
+                            WHERE I.postId = P.id and I.postId = T.postId
+                            GROUP BY P.id ORDER BY favoriteCount DESC LIMIT 4`;
 
-      const result = await db.sequelize.query(query, { type: QueryTypes.SELECT, nest: true });
-
-      for (let idx in result) {
-        const postId = result[idx]['postId'];
-
-        const tempBrief: briefInformationDTO = {
-          postId: postId,
-          title: result[idx]['title'],
-          image: '',
-          isFavorite: false,
-          tags: [],
-        };
-
-        const promise1 = new Promise(async (resolve, reject) => {
-          const query = `SELECT * 
-                                    FROM post_has_image INNER JOIN post_has_tags 
-                                    WHERE post_has_image.postId=:postId and
-                                    post_has_image.postId = post_has_tags.postId `;
-          const result = await db.sequelize.query(query, {
-            replacements: { postId: postId },
-            type: QueryTypes.SELECT,
-            nest: true,
-          });
-
-          tempBrief.image = result[0]['image1'];
-          tempBrief.tags.push(result[0]['region']);
-          tempBrief.tags.push(result[0]['theme']);
-
-          const warningTag = result[0]['warning'];
-          if (warningTag) tempBrief.tags.push(warningTag);
-
-          resolve('success');
-        });
-
-        const promise2 = new Promise(async (resolve, reject) => {
-          const query = 'select * from liked_post where PostId = :postId and UserId = :userId';
-          const ret = await db.sequelize.query(query, {
-            replacements: { postId: postId, userId: userId },
-            type: QueryTypes.SELECT,
-          });
-          if (ret.length > 0) tempBrief.isFavorite = true;
-
-          resolve('success');
-        });
-
-        await Promise.all([promise1, promise2])
-          .then(() => {
-            today.push(tempBrief);
-          })
-          .catch((err) => {
-            throw err;
-          });
-      }
-      resolve('success');
+    const todayPromise = db.sequelize.query(todayQuery, {
+      type: QueryTypes.SELECT,
+      replacements: { userId: userId, region: region },
+      raw: true,
+      nest: true,
     });
 
     await Promise.all([
@@ -162,6 +116,7 @@ export default async function mainService(userId: string, theme: string, region:
       themePromise,
       localPromise,
       initPromise,
+      todayPromise,
     ])
       .then(async (response) => {
         const bannerResult: any = response[0]; // banner
@@ -187,6 +142,9 @@ export default async function mainService(userId: string, theme: string, region:
         const initResult: any = response[5];
         main.customThemeTitle = initResult[0]['customThemeTitle'];
         main.localTitle = initResult[0]['localTitle'];
+
+        const todayResult: any = response[6]; // today
+        await makeBriefCollection(todayResult, today);
       })
       .catch((err) => {
         throw err;
